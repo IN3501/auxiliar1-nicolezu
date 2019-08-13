b6ae1f5dfff74ceeffdb1eabc2e6705223b22d07
nicolezu <nicole.zuniga.f@gmail.com>
2019-08-13 19:43:58 -0400

    agregar proyecto aux2

---
+++ auxiliares/aux 2/auxiliar2/miapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+	return render(request, 'miapp/index.html')
+
+def pestaña(request):
+	return render(request, 'miapp/pestaña.html')
+	
